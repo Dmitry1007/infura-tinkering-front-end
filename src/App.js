8151db94226e170f92d68679b50175b75bb315d9
Dmitry Sky <dmitry.sky@icloud.com>
2021-12-31 12:47:17 -0700

    add comments to explain this jibberish

--- src/App.js
+++ src/App.js
@@ -10,11 +10,23 @@ const contractAddress = "0xAf6744A0242aC65Cc886775c24990C7e4EE743ed";
 const storageContract = new web3.eth.Contract(simpleStorage, contractAddress);
 
 function App() {
+  const [number, setUint] = useState(0);
+  // Set requires gas, get does not bc get is just viewing
+  const [getNumber, setGet] = useState("0");
+  
   const numberSet = async (t) => {
     t.preventDefault();
+    // make sure we are calling our connected address via MetaMask.
     const accounts = await window.ethereum.enable();
+
+    // Pulls in the connect account
     const account = accounts[0];
+    
+    // app needs permission to access user funds to pay for gas fees
     const gas = await storageContract.methods.set(number).estimateGas();
+    
+    // going to take the passed in uint256, confirm the transaction (post paying gas fee) from your MetaMask wallet on the Ropsten network
+    // We create our smart contract transaction by passing in our function parameters to the smart contract  methods.set(), and estimated gas and user account address to .send().
     const post = await storageContract.methods.set(number).send({
       from: account,
       gas,
@@ -26,9 +38,7 @@ function App() {
     const post = await storageContract.methods.get().call();
     setGet(post);
   };
-  
-  const [number, setUint] = useState(0);
-  const [getNumber, setGet] = useState("0");
+
   return (
     <div className="App">
       <header className="App-header">
