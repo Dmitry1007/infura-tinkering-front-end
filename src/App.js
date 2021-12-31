7e18d5270c93146840cebfd46e2879d2b8b71241
Dmitry Sky <dmitry.sky@icloud.com>
2021-12-31 12:32:06 -0700

    add set/get logic

--- src/App.js
+++ src/App.js
@@ -10,6 +10,23 @@ const contractAddress = "0xAf6744A0242aC65Cc886775c24990C7e4EE743ed";
 const storageContract = new web3.eth.Contract(simpleStorage, contractAddress);
 
 function App() {
+  const numberSet = async (t) => {
+    t.preventDefault();
+    const accounts = await window.ethereum.enable();
+    const account = accounts[0];
+    const gas = await storageContract.methods.set(number).estimateGas();
+    const post = await storageContract.methods.set(number).send({
+      from: account,
+      gas,
+    });
+  };
+
+  const numberGet = async (t) => {
+    t.preventDefault();
+    const post = await storageContract.methods.get().call();
+    setGet(post);
+  };
+  
   const [number, setUint] = useState(0);
   const [getNumber, setGet] = useState("0");
   return (
